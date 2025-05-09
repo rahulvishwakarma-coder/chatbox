a35c82b2557cb03a7d3796a4c84de713702d3bf7
Rahul Vishwakarma <rahul@kaetoo.in>
2025-05-09 15:08:53 +0530

    chatsuccess

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const express = require('express');
-const path = require('path');
+import express from 'express'
+import * as path from 'path'
+import { GoogleGenAI } from "@google/genai";
+
 const app = express();
+const ai = new GoogleGenAI({ apiKey: "AIzaSyAMTrxsrwjiWUHiEJpfoMpGO-8E_FqVU_0" });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "Explain how AI works in a few words",
+  });
+  console.log(response.text);
+}
+
+main();
 
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./public'));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.post('/chat',(req,res)=>{
-  console.log(req.body);
-  res.json({reply:"hello kaise hoo"});
+app.post('/chat',async(req,res)=>{
+  const usermessage = req.body.query;
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: `${usermessage}`,
+  });
+
+  res.json({reply:response.text})
 });
 app.listen(3000, () => {
   console.log('listening on *:3000');
